75a9150a40540072d5e11237202cb33b3dea2530
devondarling <69738893+devondarling@users.noreply.github.com>
2021-03-17 17:38:24 -0400

    Templating for rooms view with JSON

--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,7 +1,10 @@
+var fs = require('fs');
+
+var room = JSON.parse(fs.readFileSync('./data/rooms.json', 'utf-8'));
 /* GET rooms view */
 const rooms = (req, res) => {
     pageTitle = process.env.npm_package_description + ' - Rooms';
-    res.render('rooms', { title: pageTitle });
+    res.render('rooms', { title: pageTitle , room});
 };
 module.exports = {
     rooms
